d8bf526b61b00a65a06938dc9ec2a8d8d75df8ce
jianghongyan <husor_jiang@eco-chain.com>
2017-09-22 14:29:56 +0800

    初始化代码

---
+++ lambda_learn2/src/main/java/com/husor/LambdaLearn2Application.java
@@ -0,0 +1,12 @@
+package com.husor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaLearn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaLearn2Application.class, args);
+	}
+}
